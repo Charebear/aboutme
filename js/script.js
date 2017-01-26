8df7d97b7742cdde66d311f9f68dea4a3643844c
Charebear <cherishgoldsby.k@gmail.com>
2017-01-26 13:49:15 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+	$("#eb-pic").hide();
+
+
+	$("#eb").click(function() {
+		$("#eb-pic").toggle();
+	});
+
+	$("#zc-pic").hide();
+
+
+	$("#zc").click(function() {
+		$("#zc-pic").toggle();
+	});
+
+	$("#jg-pic").hide();
+
+
+	$("#jg").click(function() {
+		$("#jg-pic").toggle();
+	});
+
+	$("#ah-pic").hide();
+
+
+	$("#ah").click(function() {
+		$("#ah-pic").toggle();
+	});
+
+	$("#mh-pic").hide();
+
+
+	$("#mh").click(function() {
+		$("#mh-pic").toggle();
+	});
+
+	$("#rj-pic").hide();
+
+
+	$("#rj").click(function() {
+		$("#rj-pic").toggle();
+	});
+
+	$("#tj-pic").hide();
+
+
+	$("#tj").click(function() {
+		$("#tj-pic").toggle();
+	});
+
+	$("#pl-pic").hide();
+
+
+	$("#pl").click(function() {
+		$("#pl-pic").toggle();
+	});
+
+	$("#ap-pic").hide();
+
+
+	$("#ap").click(function() {
+		$("#ap-pic").toggle();
+	});
+
+	$("#cg-pic").hide();
+
+
+	$("#cg").click(function() {
+		$("#cg-pic").toggle();
+	});
+});
